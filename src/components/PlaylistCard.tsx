0f24687300a11b495d014cec782c9fb7ee452e30
Nenad Brcic <neno@neno.ch>
2023-06-29 19:21:42 +0200

    3 instead of 4

--- src/components/PlaylistCard.tsx
+++ src/components/PlaylistCard.tsx
@@ -1,34 +1,38 @@
-import {Image} from "./Image";
-import {FC} from "react";
-import useMoviesStore from "../store/movies-store";
-import {Button} from "./Button";
-import {IconPencil, IconTrash} from "./icons";
+import { Image } from './Image';
+import { FC } from 'react';
+import useMoviesStore from '../store/movies-store';
+import { Button } from './Button';
+import { IconPencil, IconTrash } from './icons';
+import { MovieType } from '../lib/types';
 interface PlaylistCardProps {
-  id: string;
-  title: string;
-  poster_path?: string | null;
+  movie: MovieType;
 }
 
 const imageBaseUrl = 'https://image.tmdb.org/t/p/original';
-export const PlaylistCard: FC<PlaylistCardProps> = ({ id, title, poster_path }) => {
+export const PlaylistCard = ({ movie }: PlaylistCardProps) => {
+  const { id, title, poster_path, release_date, overview } = movie;
   const { deleteMovie } = useMoviesStore();
 
   return (
-    <div className="card lg:card-side lg:grid lg:grid-cols-3 bg-base-100">
+    <div className='card lg:card-side lg:grid lg:grid-cols-3 bg-base-300'>
       <figure>
-        {poster_path && <Image
-          src={`${imageBaseUrl}${poster_path}`}
-          alt={title}
-          className="w-full h-full object-center object-cover"
-          sizes='(min-width: 1280px) 640px, (min-width: 768px) 50vw, 100vw'
-          width={600}
-          height={840}
-        />}
+        {poster_path && (
+          <Image
+            src={`${imageBaseUrl}${poster_path}`}
+            alt={title}
+            className='w-full h-full object-center object-cover'
+            sizes='(min-width: 1280px) 640px, (min-width: 768px) 50vw, 100vw'
+            width={600}
+            height={840}
+          />
+        )}
       </figure>
-      <div className="card-body lg:col-span-2">
-        <h2 className="card-title">{title}</h2>
-        <p>Click the button to watch on Jetflix app.</p>
-        <div className="card-actions justify-end">
+      <div className='card-body lg:col-span-2'>
+        <h2 className='card-title'>{title}</h2>
+
+        <small>{release_date}</small>
+        <p>{overview}</p>
+        <div className='card-actions justify-end'>
           <Button
             onClick={() => deleteMovie(id)}
             title='Edit Movie'
@@ -46,5 +50,5 @@ export const PlaylistCard: FC<PlaylistCardProps> = ({ id, title, poster_path }) 
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/features/playlist/Playlist.tsx
+++ src/features/playlist/Playlist.tsx
@@ -1,5 +1,5 @@
 import useMoviesStore from '../../store/movies-store';
-import {PlaylistCard} from "../../components/PlaylistCard";
+import { PlaylistCard } from '../../components/PlaylistCard';
 
 export const Playlist = () => {
   const { movies } = useMoviesStore();
@@ -7,17 +7,10 @@ export const Playlist = () => {
   return (
     <section>
       <h2>Playlist</h2>
-      <div className='grid grid-cols-4 gap-8 bg-gray-300 shadow-xl'>
+      <div className='grid grid-cols-3 gap-8 shadow-xl'>
         {movies &&
           movies.length > 0 &&
-          movies.map((movie) => (
-            <PlaylistCard
-              key={movie.id}
-              id={movie.id}
-              title={movie.title}
-              poster_path={movie.poster_path}
-            />
-          ))}
+          movies.map((movie) => <PlaylistCard key={movie.id} movie={movie} />)}
       </div>
     </section>
   );
